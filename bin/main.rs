7a5b9230d50ab7253cbc32aa96a21d99ec963c6f
Miguel D. Salcedo <misalced@microsoft.com>
2022-01-13 15:00:55 -0500

    Use anyhow to avoid defining a CLI error.

--- bin/main.rs
+++ bin/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{AppSettings, Parser};
+use tracing::{subscriber::set_global_default, Level};
+
+#[derive(Parser)]
+#[clap(author, version, about)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::InferLongArgs))]
+#[clap(global_setting(AppSettings::InferSubcommands))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+struct Arguments {
+    #[clap(short, long, parse(from_occurrences))]
+    /// Make the subcommand more talkative.
+    verbose: usize,
+}
+
+fn main() -> Result<()> {
+    let arguments = Arguments::parse();
+
+    set_verbosity(arguments.verbose)?;
+
+    Ok(println!("Hello, world!"))
+}
+
+fn set_verbosity(occurrences: usize) -> Result<()> {
+    let level = match occurrences {
+        0 => Level::ERROR,
+        1 => Level::WARN,
+        2 => Level::INFO,
+        3 => Level::DEBUG,
+        _ => Level::TRACE,
+    };
+
+    let collector = tracing_subscriber::fmt().with_max_level(level).finish();
+
+    Ok(set_global_default(collector)?)
 }
